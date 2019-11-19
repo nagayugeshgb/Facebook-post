bf0f807fb347d2718dce634b67876e4c0b20df06
nagayugeshgb <52991783+nagayugeshgb@users.noreply.github.com>
2019-11-19 18:47:51 +0530

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    state = {  }
+    render() { 
+        return (
+            <div>
+        <nav className="navbar navbar-light bg-primary" >
+        <span className="navbar-brand mb-0 h1"><h2>Write Something</h2></span></nav>
+    </div>
+          );
+    }
+}
+ 
+export default Navbar;
+
